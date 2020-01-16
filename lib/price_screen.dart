cceea1f769b2a14c034c1db6776c44b03fe84428
mudassar7799 <ch.mudassar779@gmail.com>
2020-01-16 09:35:34 +0500

    add

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -5,8 +5,12 @@ class PriceScreen extends StatefulWidget {
   _PriceScreenState createState() => _PriceScreenState();
 }
 
-class _PriceScreenState extends State<PriceScreen> {
-  @override
+class _PriceScreenState extends State<PriceScreen>
+
+ String selectedCurrency = 'USD';
+
+  {
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -42,7 +46,21 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items:[
+              DropdownMenuItem(child: Text('USD'),
+                value: 'USD',
+              ),
+              DropdownMenuItem(child: Text('EUR'),
+                value: 'EUR',
+              ),
+              DropdownMenuItem(child: Text('GBP'),
+                value: 'GBP',
+              )
+            ],onChanged : (value){
+              selectedCurrency = value;
+            }),
           ),
         ],
       ),
